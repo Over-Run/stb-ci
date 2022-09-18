259fdc1e8ea2f5ecd7ad5db6f0819926f82ef3cc
squid233 <60126026+squid233@users.noreply.github.com>
2022-09-18 13:16:49 +0800

    Update stb.c

--- stb.c
+++ stb.c
@@ -4,3 +4,22 @@
 #include "stb_image_resize.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+#ifndef STB_C_DLLEXPORT
+#ifdef _MSC_VER
+#define STB_C_DLLEXPORT extern __declspec(dllexport)
+#else
+#define STB_C_DLLEXPORT extern
+#endif
+#endif
+
+extern "C" {
+
+STB_C_DLLEXPORT int stbi_get_write_tga_with_rle() { return stbi_write_tga_with_rle; }
+STB_C_DLLEXPORT int stbi_get_write_png_compression_level() { return stbi_write_png_compression_level; }
+STB_C_DLLEXPORT int stbi_get_write_force_png_filter() { return stbi_write_force_png_filter; }
+STB_C_DLLEXPORT void stbi_set_write_tga_with_rle(int v) { stbi_write_tga_with_rle = v; }
+STB_C_DLLEXPORT void stbi_set_write_png_compression_level(int v) { stbi_write_png_compression_level = v; }
+STB_C_DLLEXPORT void stbi_set_write_force_png_filter(int v) { stbi_write_force_png_filter = v; }
+
+}
