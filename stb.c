c80dbee1c00068c369460807f49b0d5bfe1e345a
squid233 <60126026+squid233@users.noreply.github.com>
2024-02-23 23:10:07 +0800

    update

--- stb.c
+++ stb.c
@@ -12,26 +12,3 @@
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 #include "stb_vorbis.c"
-
-#ifndef STB_C_DLLEXPORT
-#ifdef _MSC_VER
-#define STB_C_DLLEXPORT extern __declspec(dllexport)
-#else
-#define STB_C_DLLEXPORT extern
-#endif
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-STB_C_DLLEXPORT int stbi_get_write_tga_with_rle() { return stbi_write_tga_with_rle; }
-STB_C_DLLEXPORT int stbi_get_write_png_compression_level() { return stbi_write_png_compression_level; }
-STB_C_DLLEXPORT int stbi_get_write_force_png_filter() { return stbi_write_force_png_filter; }
-STB_C_DLLEXPORT void stbi_set_write_tga_with_rle(int v) { stbi_write_tga_with_rle = v; }
-STB_C_DLLEXPORT void stbi_set_write_png_compression_level(int v) { stbi_write_png_compression_level = v; }
-STB_C_DLLEXPORT void stbi_set_write_force_png_filter(int v) { stbi_write_force_png_filter = v; }
-
-#ifdef __cplusplus
-}
-#endif
